420412d3eb26ac185f70fc00a1bd7353b83497c2
Pelayo Fernández <fernandezpelayo@hotmail.es>
2026-03-18 14:43:05 +0100

    Rutas automatizadas con pathlib

--- scripts/database_upload.py
+++ scripts/database_upload.py
@@ -2,14 +2,16 @@ import pandas as pd
 import os
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
+from pathlib import Path
 
-load_dotenv()
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_PATH = BASE_DIR / "data" / "mantenimiento.csv"
+ENV_PATH = BASE_DIR / ".env"
+
+load_dotenv(dotenv_path=ENV_PATH)
 PASSWORD = os.getenv('DB_PASSWORD')
 
-try:
-    df = pd.read_csv('data/mantenimiento.csv')
-except FileNotFoundError:
-    df = pd.read_csv('mantenimiento.csv')
+df = pd.read_csv(DATA_PATH)
 
 df_mantenimiento = df.drop(df.columns[[0, 1]], axis=1)
 df_mantenimiento.columns = ['Tipo', 'Temp_Aire', 'Temp_Proceso', 'Velocidad', 'Torque', 'Desgaste', 'Fallo', 'Tipo_Fallo']
@@ -24,4 +26,4 @@ with engine.connect() as conn:
 
 df_mantenimiento.to_sql('monitoreo_maquinas', con=engine, schema='mantenimiento_industrial', if_exists='replace', index=False)
 
-print("EXITO: Datos en MySQL.")
+print("EXITO: Datos cargados correctamente desde cualquier ubicación.")
